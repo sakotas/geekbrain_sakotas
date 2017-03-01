e7f2c4d30a5f3c5d66576a57f74bbefba0ecabb3
Alex Kotishevskiy <alexkotishevskiy@MacBook-Air-Alex.local>
2017-03-01 15:57:30 +0300

    no message

--- Lesson2.1Homework/main.m
+++ Lesson2.1Homework/main.m
@@ -11,13 +11,6 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        
-        int firstVar = 5;
-        int secondVar = 6;
-        int result = firstVar + secondVar;
-        NSLog(@"%i", result);
-        
-        
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
